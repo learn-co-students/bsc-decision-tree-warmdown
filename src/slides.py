a6ee8590af9d6c4a1d72b36bc6187b4e97215ef2
Joél Collins <joelsewhere@gmail.com>
2021-07-13 16:23:08 -0500

    Initial

---
+++ src/slides.py
@@ -0,0 +1,4 @@
+from IPython.display import HTML
+
+slides = HTML('<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRjpSr7iFUUJLV8RrrLtBF3czpuhV4iKnAylzbhDLlrvYirUO6hmNxyEerJjC8uTyIbA8WpDN1ZvnHR/embed?start=false&loop=false&delayms=3000" frameborder="0" width="500" height="300" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>')
+
